0471aaf59f046bef094857b31258985275fc705e
Maria Naeem Khokhar <114730306+mariathv@users.noreply.github.com>
2025-02-02 13:20:41 +0500

    Backend structure defined

--- Express-Backend/app.js
+++ Express-Backend/app.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//--- parsing middlewares ---
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const authRouter = require("./routes/auth.router");
+const usersRouter = require("./routes/users.router");
+const gamesRouter = require("./routes/games.router");
+
+//--- defining endpoints ---
+//app.use("/api/v1/auth", authRouter);
+//app.use("/api/v1/users", usersRouter);
+//app.use("/api/v1/games", gamesRouter);
 
 app.get('/', (req, res) => {
     res.send('Server Running')
 })
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
