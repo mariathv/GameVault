498eeb82ce79de6b48f2e232c3c2b2507cca1663
mariathv <naeemmaria60@gmail.com>
2025-05-05 12:21:14 -0700

    deployment: vercel token bug fix

--- React-Frontend/vite.config.js
+++ React-Frontend/vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
   },
   server: {
     port: 3000, 
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
   },
 
 
